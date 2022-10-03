b8c7747330e3e002015b14b00d808deab70d54d1
Ramoshtml <114829780+Ramoshtml@users.noreply.github.com>
2022-10-03 18:44:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+var edad == alert(prompt("Cual es su edad?"));
+
+if(edad >= 18 ) { 
+alert("sos mayor de edad podes entrar");	
+window.location.href=('index.html');
+}else{ 
+alert("sos menor de edad");
+window.location.href = ('https://www.google.com');
+}
